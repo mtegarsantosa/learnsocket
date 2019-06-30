0f4f4649bf9c9e38cb8fc786ce2e0235ad8ee663
ARSAN <tganteng77@gmail.com>
2019-06-30 16:24:11 +0700

    change port

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 
-http.listen(4000, ()=>{
-  console.log('port 4000')
+http.listen(3000, ()=>{
+  console.log('listened on port 3000')
 })
 
 app.use('/', (req,res)=>{
